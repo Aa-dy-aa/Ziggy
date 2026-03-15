0faec507f808c176835c6483555c351fc9962d6b
Aadyaa Sundriyal <sundriyalaadyaa77@gmail.com>
2026-03-15 17:35:03 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
  import './App.css';
 import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Login from './screens/Login';
 
 function App() {
   return (
-    <>
-      <div><Home/></div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">Navbar</a>
+          <Link className="navbar-brand fs-1 fst-italic" to="/">Ziggy</Link>
 
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="/navbarSupportedContent"
+            data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
@@ -23,35 +23,12 @@ function Navbar() {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <Link className="nav-link active" to="/">Home</Link>
               </li>
 
               <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
+                <Link className="nav-link" to="/login">Login</Link>
               </li>
-
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                >
-                  Dropdown
-                </a>
-
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="/">Action</a></li>
-                  <li><a className="dropdown-item" href="/">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="/">Something else here</a></li>
-                </ul>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-
             </ul>
 
             <form className="d-flex">
@@ -60,7 +37,7 @@ function Navbar() {
                 type="search"
                 placeholder="Search"
               />
-              <button className="btn btn-outline-success">
+              <button className="btn btn-outline-success" type="submit">
                 Search
               </button>
             </form>
@@ -69,7 +46,7 @@ function Navbar() {
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
