e7abc38864b9613668c89ce99274ce52292b8ab9
Aadyaa Sundriyal <sundriyalaadyaa77@gmail.com>
2026-03-15 21:45:56 +0530

    Navbar and footer done

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
 
 function Home() {
   return (
     <div>
-        <div><Navbar /></div>
-        <div>Home</div>
-        <div><Footer /></div>
+      <div><Navbar /></div>
+      <div><Card /></div>
+      <div><Footer /></div>
     </div>
   )
 }
