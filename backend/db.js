011c39cc787cd64498618008fd2f794d4374d000
Aadyaa Sundriyal <sundriyalaadyaa77@gmail.com>
2026-03-19 23:29:34 +0530

    commit

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const mongoURI = process.env.MONGO_URI;
 const mongoDB = async () => {
     try {
         await mongoose.connect(mongoURI);
-        console.log("✅ Connected to MongoDB");
+        console.log(" Connected to MongoDB");
 
         const fetched_data = mongoose.connection.db.collection('food_items');
 
@@ -13,7 +13,7 @@ const mongoDB = async () => {
         console.log(data);
 
     } catch (error) {
-        console.error("❌ MongoDB connection failed:", error);
+        console.error(" MongoDB connection failed:", error);
     }
 };
 
